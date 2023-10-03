8e29f186fd503b3e837d92c67ae8f0037b27b941
Huy Nguyễn <khoidauytb@gmail.com>
2023-10-03 07:30:12 +0700

    #61 update 4 fields updateUserData()

--- src/services/userService.js
+++ src/services/userService.js
@@ -133,7 +133,7 @@ class userService {
     updateUserData(data) {
         return new Promise(async (resolve, reject) => {
             try {
-                if (!data.id) {
+                if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                     resolve({
                         errCode: 2,
                         errMessage: 'Missing required parameter!',
@@ -147,6 +147,10 @@ class userService {
                     user.firstName = data.firstName;
                     user.lastName = data.lastName;
                     user.address = data.address;
+                    user.roleId = data.roleId;
+                    user.positionId = data.positionId;
+                    user.gender = data.gender;
+                    user.phoneNumber = data.phoneNumber;
 
                     await user.save();
 
