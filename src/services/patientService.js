d037a349db739003e7bf22cb38340335d6e98640
Huy Nguyen <nqhuysmbg@gmail.com>
2023-11-23 15:40:12 +0700

    #85 add điều kiện gửi request

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -4,8 +4,7 @@ class PatientService {
     postBookAppointment(data) {
         return new Promise(async (resolve, reject) => {
             try {
-                if (!data.email) {
-                    //  || !data.doctorId || !data.timeType || !data.date
+                if (!data.email || !data.doctorId || !data.timeType || !data.date) {
                     resolve({
                         errCode: 1,
                         errMessage: 'Missing required parameter',
